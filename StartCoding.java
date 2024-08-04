0d9b33b4aba048543d0565546d4fc89b33b7fcb8
Aakash Vetiraayan <aakashvallamkonda@gmail.com>
2024-08-04 23:00:05 +0530

    Create StartCoding.java
    When learning a new language, we first learn to output some message. Here, we'll start with the famous Hello World message. Now, here you are given a function to complete. Don't worry about the ins and outs of functions, just add the command (System.out.print("Hello World")) to print Hello World.
    
    Example:
    
    Input:
    No input
    
    Output:
    Hello World
    
    Explanation:
    Hello World is printed.
    Expected Time Complexity: O(1).
    Expected Auxiliary Space: O(1).
    
    User Task:
    Your task is to complete the function below to print Hello World.

---
+++ StartCoding.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+
+
+###When learning a new language, we first learn to output some message. Here, we'll start with the famous Hello World message. Now, here you are given a function to complete. Don't worry about the ins and outs of functions, just add the command (System.out.print("Hello World")) to print Hello World.
+
+Example:
+
+Input:
+No input
+
+Output:
+Hello World
+
+Explanation:
+Hello World is printed.
+Expected Time Complexity: O(1).
+Expected Auxiliary Space: O(1).
+
+User Task:
+Your task is to complete the function below to print Hello World.**
+
+class Geeks{
+    
+    // Function to print hello
+    static void printHello(){
+        
+        System.out.println("Hello World");
+        
+        
+    }
+    
+    public static void main (String[]args){
+        printHello();
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+class GfG{
+    
+    public static void main(String args[]){
+       
+       //Creating an Object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling printHello() function of the Class Geeks
+        g.printHello();
+    }
+    
+}
+// } Driver Code Ends
